d1139d866967d1d0db5fb7559038cbfc4ef6952e
Kinga Kotowicz <kingkot226@student.polsl.pl>
2022-03-21 15:16:25 +0100

    main page layout

--- app/src/main/java/com/example/pogodynka_kotowicz_kinga/MainActivity.kt
+++ app/src/main/java/com/example/pogodynka_kotowicz_kinga/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.pogodynka_kotowicz_kinga
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+
     }
 }
